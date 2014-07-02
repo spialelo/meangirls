d5124e506ed8ea6a5f7214b8a950e85ee129f53f
Nicole Phillips <ndphillips@gmail.com>
2014-07-02 02:47:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meangirls::Application.config.secret_key_base = '7b8f82411d67c9e764275491c8e0ada787cc0f03e20a9ca63c52cc2830efc5196db4c3ff5f7273cd13702db728ac68db68cd541c9de07357ab0d3367f475dc82'
