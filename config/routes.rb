e3eda34d6a2c79706e5769d34b0d2e8ca3933229
Nicole Phillips <ndphillips@gmail.com>
2014-07-02 03:29:04 +0000

    Updated quotes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Meangirls::Application.routes.draw do
+root 'quotes#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
